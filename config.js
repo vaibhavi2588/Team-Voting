254ffa878c6e2d789ee7cf6d0bbac87e412aa1c9
vaibhavi2588 <72645558+vaibhavi2588@users.noreply.github.com>
2021-11-07 10:36:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCtMMSB0csleVxYEXygfZ9CjjZtRyQ-qS8",
+  authDomain: "project--67.firebaseapp.com",
+  projectId: "project--67",
+  storageBucket: "project--67.appspot.com",
+  messagingSenderId: "179950982610",
+  appId: "1:179950982610:web:fbf72e2988b79eee87a40b"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
